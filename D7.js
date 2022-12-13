1ca483b6b126c002bb603d9fbdcc851566e82993
Marco-Sorrentino <sorrentmarco@gmail.com>
2022-12-13 19:47:16 +0100

    exercise 8

--- D7.js
+++ D7.js
@@ -64,6 +64,18 @@ function arraySomma(ar) {
 
 console.log(arraySomma([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
 
+/*
+const arraySomma = ar => {
+  let somma = 0
+  for ( let i = 0 ; i < ar.length ; i++) {
+    somma += ar[i]
+  }
+  return somma
+}
+
+console.log(arraySomma([1,2,3,4,5,6,7,8,9,10]))
+*/
+
 /* ESERCIZIO 5
   Scrivi una funzione per sommare i numeri contenuti in un array (usare REDUCE)
 */
@@ -80,10 +92,48 @@ console.log(somma);
   Scrivi una funzione che, dato un array di soli numeri e un numero n come parametri, ritorni un secondo array con tutti i valori del precedente incrementati di n
 */
 
+const arrayDiNumeri = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function incremento(n) {
+  const nuovoArray = [];
+  for (let i = 0; i < arrayDiNumeri.length; i++) {
+    nuovoArray.push(arrayDiNumeri[i] + n);
+  }
+  return nuovoArray;
+}
+
+console.log(incremento(3));
+
+/*
+const arrayDiNumeri = [1,2,3,4,5,6,7,8,9,10]
+
+const arraySommato = x => {
+  const newArray = []
+  for ( let i = 0 ; i < arrayDiNumeri.length ; i++) {
+    newArray.push(arrayDiNumeri[i] + x)
+  }
+  return newArray
+}
+
+console.log(arraySommato(10))
+*/
+
 /* ESERCIZIO 8
   Scrivi una funzione che, dato un array di stringhe, ritorni un nuovo array contenente le lunghezze delle rispettive stringhe dell'array di partenza
   es.: ["EPICODE", "is", "great"] => [7, 2, 5]
 */
+
+const stringArray = ["ciao", "mi", "chiamo", "marco"];
+
+function convertInNumber() {
+  const numberArray = [];
+  for (let i = 0; i < stringArray.length; i++) {
+    numberArray.push(stringArray[i].length);
+  }
+  return numberArray;
+}
+
+console.log(convertInNumber());
 
 /* ESERCIZIO 9
   Scrivi una funzione per creare un array contenente tutti i valori DISPARI da 1 a 99.
